a099b696b3eebc434b4e62d80e7aa05aa702423c
yuji.sakamoto <yuji.sakamoto@fulldepth.co.jp>
2026-01-16 16:51:10 +0900

    推力方向を進行方向に逆転しM5/M6の向きを逆転しpitch/yawを無効化+M5/M6のForward成分を追加：動作OKだけど設定したいフレームとは違う

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -176,16 +176,16 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
         break;
 
     case SUB_FRAME_CUSTOM: {
-        _frame_class_string = "HAYATE_VECTORED_SIGNFIX";
-        //                 Motor #          Roll  Pitch Yaw   Throttle Forward Lateral Testing Order
-        // 水平4基：Yaw/Forward/Lateral のみ（Climb=0）
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,  0.0f, 0.0f, +1.0f,  0.0f, -0.866f, +0.5f, 1); // M1: bow-starboard (ψ=-30°) → F=-0.866, L=+0.5,  Yaw=+1
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,  0.0f, 0.0f, -1.0f,  0.0f, -0.866f, -0.5f, 2); // M2: bow-port (ψ=+30°) → F=-0.866, L=-0.5,  Yaw=-1
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,  0.0f, 0.0f, -1.0f,  0.0f, +0.866f, +0.5f, 3); // M3: stern-starboard (ψ=+30°) → F=+0.866, L=+0.5,  Yaw=-1
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,  0.0f, 0.0f, +1.0f,  0.0f, +0.866f, -0.5f, 4); // M4: stern-port (ψ=-30°) → F=+0.866, L=-0.5,  Yaw=+1
-        // 縦2基：Climb（Down+）のみ
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,  0.0f, 0.0f, 0.0f,  -1.0f, 0.0f, 0.0f, 5);
-        add_motor_raw_6dof(AP_MOTORS_MOT_6,  0.0f, 0.0f, 0.0f,  -1.0f, 0.0f, 0.0f, 6);
+        _frame_class_string = "HAYATE_M5M6_add_Forward";
+        //                 Motor #           Roll Pitch  Yaw  Throttle Forward Lateral Testing Order
+        // 水平4基（ψ = ±30°）
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,  0.0f, 0.0f, +1.0f,  0.0f, -0.866f, +0.5f, 1); // bow-starboard
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,  0.0f, 0.0f, -1.0f,  0.0f, -0.866f, -0.5f, 2); // bow-port
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,  0.0f, 0.0f, -1.0f,  0.0f, +0.866f, +0.5f, 3); // stern-starboard
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,  0.0f, 0.0f, +1.0f,  0.0f, +0.866f, -0.5f, 4); // stern-port
+        // 縦2基（前傾 θ=-34°）
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,  0.0f, 0.0f, 0.0f,  -1.0f, +0.829f, 0.0f, 5); // vertical starboard
+        add_motor_raw_6dof(AP_MOTORS_MOT_6,  0.0f, 0.0f, 0.0f,  -1.0f, +0.829f, 0.0f, 6); // vertical port
         break;
     }
 
